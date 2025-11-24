dc7497ad1dabda2dfbfb0f83b3758912ac87ea96
nurlan2209 <nkuanyshuly2020@gmail.com>
2025-11-25 04:25:49 +0500

    gotovvh

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -6,8 +6,8 @@ class ApiService {
     _dio = Dio(
       BaseOptions(
         baseUrl: apiBaseUrl,
-        connectTimeout: const Duration(seconds: 15),
-        receiveTimeout: const Duration(seconds: 15),
+        connectTimeout: const Duration(seconds: 30),
+        receiveTimeout: const Duration(seconds: 30),
         headers: {'Content-Type': 'application/json'},
       ),
     );
